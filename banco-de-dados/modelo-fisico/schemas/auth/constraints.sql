1c4ce288652581969eddd11f8f555a666febffc5
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2025-05-31 16:32:01 -0300

    Exclusão de constraints para "uf"
    Essas constraints foram excluídas, pois foram substituídas pelo domain "domain_uf"

--- banco-de-dados/modelo-fisico/schemas/auth/constraints.sql
+++ banco-de-dados/modelo-fisico/schemas/auth/constraints.sql
@@ -1,13 +1,3 @@
--- valida o "uf"
-ALTER TABLE auth.tb_endereco
-ADD CONSTRAINT
-check_schema_auth_tb_endereco_uf
-CHECK (uf IN (
-  'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO',
-  'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR',
-  'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'
-));
-
 -- valida o "sexo"
 ALTER TABLE auth.tb_usuario
 ADD CONSTRAINT
--- banco-de-dados/modelo-fisico/schemas/church/constraints.sql
+++
@@ -1,9 +0,0 @@
--- valida o "uf"
-ALTER TABLE church.tb_igreja
-ADD CONSTRAINT
-check_schema_church_tb_igreja_uf
-CHECK (uf IN (
-  'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO',
-  'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR',
-  'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'
-));
