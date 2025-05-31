48c5ed3d5ac0b64fab294fdae0f07ec90cc7f50d
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2025-05-31 15:00:34 -0300

    Alteração do nome de check constraints

--- banco-de-dados/modelo-fisico/schemas/auth/constraints.sql
+++ banco-de-dados/modelo-fisico/schemas/auth/constraints.sql
@@ -1,7 +1,7 @@
 -- valida o "uf"
 ALTER TABLE auth.tb_endereco
 ADD CONSTRAINT
-schema_auth_tb_endereco_constraint_validar_uf
+check_schema_auth_tb_endereco_uf
 CHECK (uf IN (
   'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO',
   'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR',
@@ -11,5 +11,7 @@ CHECK (uf IN (
 -- valida o "sexo"
 ALTER TABLE auth.tb_usuario
 ADD CONSTRAINT
-schema_auth_tb_endereco_constraint_validar_sexo
-CHECK (sexo IN ('M', 'F'));
+check_schema_auth_tb_endereco_sexo
+CHECK (sexo IN (
+  'M', 'F'
+));
