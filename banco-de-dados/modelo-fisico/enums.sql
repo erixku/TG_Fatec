0cf2e80a8cb2a95f1bc1ac094a64e220471cbc3b
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2025-05-31 21:11:48 -0300

    Adição de ENUM para denominações de igreja (schema church)

--- banco-de-dados/modelo-fisico/enums.sql
+++ banco-de-dados/modelo-fisico/enums.sql
@@ -1,3 +1,4 @@
+-- SCHEMA STORAGE
 CREATE TYPE
 enum_schema_storage_tb_arquivo_mime_type
 AS ENUM (
@@ -12,4 +13,41 @@ AS ENUM (
   'png', 'jpg', 'jpeg', 'svg',
   'mp3', 'wav', 'ogg',  'flac', 'm4a', 'alac',
   'pdf'
+);
+
+-- SCHEMA CHURCH
+CREATE TYPE
+enum_schema_church_tb_igreja_denominacao
+AS ENUM (
+  'Adventista',
+  'Assembleia de Deus',
+  'Batista',
+  'Bola de Neve',
+  'Casa da Bênção',
+  'Comunidade Cristã',
+  'Congregação Cristã no Brasil',
+  'Deus é Amor',
+  'Evangelho Quadrangular',
+  'Igreja Episcopal',
+  'Igreja Internacional da Graça de Deus',
+  'Igreja Luterana',
+  'Igreja Metodista',
+  'Igreja Pentecostal',
+  'Igreja Presbiteriana',
+  'Igreja Renascer em Cristo',
+  'Igreja Sara Nossa Terra',
+  'Igreja Universal do Reino de Deus',
+  'Ministério Apascentar',
+  'Ministério Fonte da Vida',
+  'Ministério Internacional da Restauração',
+  'Ministério Voz da Verdade',
+  'Nova Vida',
+  'O Brasil Para Cristo',
+  'Paz e Vida',
+  'Projeto Vida',
+  'Verbo da Vida',
+  'Videira',
+  'Vitória em Cristo',
+  'Wesleyana',
+  'Zion Church'
 );
