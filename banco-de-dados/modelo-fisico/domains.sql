7e87cf7dcbbe135ffe8155396e23c731fb3695b8
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2025-05-31 21:12:42 -0300

    Adição de domínio para tipar campos de "uf"

---
+++ banco-de-dados/modelo-fisico/domains.sql
@@ -0,0 +1,9 @@
+CREATE DOMAIN domain_uf
+AS CHAR(2)
+CHECK (
+  VALUE IN (
+    'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO',
+    'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR',
+    'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'
+  )
+);
