ca4180d3e8a03021ace6ee4926485f55bd685222
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2025-06-08 01:49:26 -0300

    Adição de CHECK CONSTRAINTS à tabela "storage.tb_bucket"
    Constraints foram adicionadas a essa tabela para validar a duração dos uploads, de forma que durem de mais que zero segundos até, no máximo, cinco minutos. Além disso, foram criadas validações para manter tamanhos mínimos e máximos para arquivos dentro de um bucket, impedindo que arquivos menores ou maiores que o tamanho solicitado sejam armazenados, como também proibindo que o tamanho mínimo do bucket seja maior que o tamanho máximo dele

---
+++ banco-de-dados/modelo-fisico/schemas/storage/constraints.sql
@@ -0,0 +1,23 @@
+-- garante que o upload de arquivos
+-- dure de mais que zero a no máximo 5 minutos
+ALTER TABLE storage.tb_bucket
+ADD CONSTRAINT
+check_schema_storage_tb_bucket_tempo_expiracao_upload_em_segundos
+CHECK (
+  tempo_expiracao_upload_em_segundos > 0 AND
+  tempo_expiracao_upload_em_segundos <= 300
+);
+
+-- garante que o tamanho mínimo e máximo
+-- dos arquivos esteja no intervalo de
+-- 1 byte a 1 gigabyte
+ALTER TABLE storage.tb_bucket
+ADD CONSTRAINT
+check_schema_storage_tb_bucket_tamanho_minimo_e_maximo
+CHECK (
+  tamanho_minimo > 0 AND
+  tamanho_minimo <= 1073741824 AND
+  tamanho_maximo > 0 AND
+  tamanho_maximo <= 1073741824 AND
+  tamanho_maximo >= tamanho_minimo
+);
