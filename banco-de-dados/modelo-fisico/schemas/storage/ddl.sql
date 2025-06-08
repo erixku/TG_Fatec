188a0369ccb706a28f979d5eb767e090fe74d088
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2025-06-08 01:45:32 -0300

    Adição da tabela "storage.tb_bucket" , correções gerais e adição de UNIQUE KEYS

--- banco-de-dados/modelo-fisico/schemas/storage/ddl.sql
+++ banco-de-dados/modelo-fisico/schemas/storage/ddl.sql
@@ -3,6 +3,24 @@ CREATE SCHEMA storage;
 
 
 
+CREATE TABLE storage.tb_bucket (
+  -- chaves primárias
+  id SMALLINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+  -- dados de logs
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP WITH TIME ZONE     NULL DEFAULT NULL,
+
+  -- dados do bucket
+  nome                               VARCHAR(20) NOT NULL UNIQUE,
+  tempo_expiracao_upload_em_segundos SMALLINT    NOT NULL DEFAULT 30,
+  tamanho_minimo                     INTEGER     NOT NULL DEFAULT 1,
+  tamanho_maximo                     INTEGER     NOT NULL DEFAULT 1073741824
+);
+
+
+
 CREATE TABLE storage.tb_arquivo (
   -- chaves primárias
   id   INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -10,14 +28,24 @@ CREATE TABLE storage.tb_arquivo (
 
   -- dados de logs
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  deleted_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP WITH TIME ZONE     NULL DEFAULT NULL,
 
   -- dados do arquivo
   deletado         BOOLEAN                                  NOT NULL DEFAULT false,
   caminho          TEXT                                     NOT NULL,
   mime_type        enum_schema_storage_tb_arquivo_mime_type NOT NULL,
   extensao         enum_schema_storage_tb_arquivo_extensao  NOT NULL,
-  tamanho_em_bytes INTEGER                                  NOT NULL
+  tamanho_em_bytes INTEGER                                  NOT NULL,
+
+  -- chaves estrangeiras
+  buc_id SMALLINT NOT NULL,
+
+  -- declaração de chaves estrangeiras
+  CONSTRAINT fk_bucket_id
+    FOREIGN KEY (buc_id)
+    REFERENCES storage.tb_bucket (id)
+    ON UPDATE RESTRICT
+    ON DELETE RESTRICT
 );
 
 
@@ -27,15 +55,15 @@ CREATE TABLE storage.tb_arquivo_ass_usuario (
   id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 
   -- chaves estrangeiras
-  arq_id                  UUID NOT NULL,
+  arq_uuid                UUID NOT NULL,
   schema_auth_usuario_lev UUID NOT NULL,
 
   -- chaves únicas compostas
-  UNIQUE (arq_id, schema_auth_usuario_lev),
+  UNIQUE (arq_uuid, schema_auth_usuario_lev),
 
   -- declaração de chaves estrangeiras
   CONSTRAINT fk_arq_uuid
-    FOREIGN KEY (arq_id)
+    FOREIGN KEY (arq_uuid)
     REFERENCES storage.tb_arquivo (uuid)
     ON UPDATE RESTRICT
     ON DELETE RESTRICT,
