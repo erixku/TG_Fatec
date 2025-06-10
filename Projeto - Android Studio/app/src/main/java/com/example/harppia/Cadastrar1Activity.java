6ed3f75cc22532a1c2d3818b45f22921af7c7c93
Erick Gomes Barbosa <erickgomes1010@outlook.com>
2025-06-09 23:32:16 -0300

    Criação do layout da tela de cadastro

--- Projeto - Android Studio/app/src/main/java/com/example/harppia/Cadastrar1Activity.java
+++ Projeto - Android Studio/app/src/main/java/com/example/harppia/Cadastrar1Activity.java
@@ -1,6 +1,8 @@
 package com.example.harppia;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuActivity extends AppCompatActivity {
+public class Cadastrar1Activity extends AppCompatActivity {
+
+    ImageView ivVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_cadastrar1);
+
+        ivVoltar = findViewById(R.id.ivVoltar);
+
+        ivVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.entrada_esquerda, R.anim.saida_direita);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
