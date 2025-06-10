d251134f0a70b2e8d0f11ca0306b342a0547b19b
Erick Gomes Barbosa <erickgomes1010@outlook.com>
2025-06-09 23:27:15 -0300

    Adicção de botões de navegação apara Entrar ou Cadastrar

--- Projeto - Android Studio/app/src/main/java/com/example/harppia/InicioActivity.java
+++ Projeto - Android Studio/app/src/main/java/com/example/harppia/InicioActivity.java
@@ -1,6 +1,9 @@
 package com.example.harppia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class InicioActivity extends AppCompatActivity {
 
+    Button btEntrar, btCadastrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inicio);
+
+        btEntrar = findViewById(R.id.btEntrar);
+        btCadastrar = findViewById(R.id.btCadastrar);
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent entrar = new Intent(InicioActivity.this, Cadastrar1Activity.class);
+                startActivity(entrar);
+                overridePendingTransition(R.anim.entrada_direita, R.anim.saida_esquerda);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
