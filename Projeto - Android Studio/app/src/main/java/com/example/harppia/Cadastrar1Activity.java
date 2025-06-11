46bf6aa3750e8fc523ef9c634a35b00888f9aef6
Erick Gomes Barbosa <erickgomes1010@outlook.com>
2025-06-10 21:06:20 -0300

    Estilizando e adicionando funcionalidades na tela de cadastro

--- Projeto - Android Studio/app/src/main/java/com/example/harppia/Cadastrar1Activity.java
+++ Projeto - Android Studio/app/src/main/java/com/example/harppia/Cadastrar1Activity.java
@@ -1,8 +1,14 @@
 package com.example.harppia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +19,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class Cadastrar1Activity extends AppCompatActivity {
 
     ImageView ivVoltar;
+    AutoCompleteTextView ddSexo;
+    Button btProximo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,9 @@ public class Cadastrar1Activity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastrar1);
 
         ivVoltar = findViewById(R.id.ivVoltar);
+        ddSexo = findViewById(R.id.ddSexo);
+        popularEColetarSexo();
+        btProximo = findViewById(R.id.btProximo);
 
         ivVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,10 +41,38 @@ public class Cadastrar1Activity extends AppCompatActivity {
             }
         });
 
+        btProximo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent prox = new Intent(Cadastrar1Activity.this, Cadastro2Activity.class);
+                startActivity(prox);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.entrada_esquerda, R.anim.saida_direita);
+    }
+
+    private void popularEColetarSexo() {
+        String sexo[] = {"Masculino", "Feminino"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.dropdown_item, sexo);
+        ddSexo.setAdapter(adapter);
+        ddSexo.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String sexoSelecionado = ddSexo.getText().toString();
+                Toast.makeText(Cadastrar1Activity.this, "Sexo selecionado: " + sexoSelecionado, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
