15cc2ea8409f631e1891db95de641f6a514b574b
GustavoSouza417 <gustavosouza.pro417@gmail.com>
2025-06-17 21:14:17 -0300

    Alocando funções do schema "public" para o schema "app_utills"

--- banco-de-dados/modelo-fisico/schemas/storage/dml.sql
+++ banco-de-dados/modelo-fisico/schemas/storage/dml.sql
@@ -4,13 +4,13 @@ INSERT INTO storage.tb_bucket (
   tamanho_minimo,
   tamanho_maximo
 ) VALUES 
-  ('foto-perfil-usuario',       30, 1, get_tamanho_em_mb(5)),
-  ('foto-perfil-igreja',        30, 1, get_tamanho_em_mb(5)),
-  ('foto-perfil-ministerio',    30, 1, get_tamanho_em_mb(5)),
-  ('icone-instrumento',         30, 1, get_tamanho_em_mb(5)),
-  ('audio',                     30, 1, get_tamanho_em_mb(150)),
-  ('pdf',                       30, 1, get_tamanho_em_mb(5)),
+  ('foto-perfil-usuario',       30, 1, app_utils.get_tamanho_em_mb(5)),
+  ('foto-perfil-igreja',        30, 1, app_utils.get_tamanho_em_mb(5)),
+  ('foto-perfil-ministerio',    30, 1, app_utils.get_tamanho_em_mb(5)),
+  ('icone-instrumento',         30, 1, app_utils.get_tamanho_em_mb(5)),
+  ('audio',                     30, 1, app_utils.get_tamanho_em_mb(150)),
+  ('pdf',                       30, 1, app_utils.get_tamanho_em_mb(5)),
 
-  ('sistema-icone-instrumento', 30, 1, get_tamanho_em_mb(5)),
-  ('sistema-imagem',            30, 1, get_tamanho_em_mb(5)),
-  ('sistema-audio',             30, 1, get_tamanho_em_mb(5));
+  ('sistema-icone-instrumento', 30, 1, app_utils.get_tamanho_em_mb(5)),
+  ('sistema-imagem',            30, 1, app_utils.get_tamanho_em_mb(5)),
+  ('sistema-audio',             30, 1, app_utils.get_tamanho_em_mb(5));
