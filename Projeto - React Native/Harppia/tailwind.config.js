f87fb0974caa262d356805cfe719c89ca67f6d3f
erick-gomes-barbosa <erick.barbosa@baymetrics.com.br>
2025-07-27 22:14:45 -0300

    Instalação e configuração das fontes do projeto na aplicação

--- Projeto - React Native/Harppia/tailwind.config.js
+++ Projeto - React Native/Harppia/tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        //Nunito
+        'nunito-light': ['Nunito-Light'],
+        'nunito': ['Nunito-Regular'],
+        'nunito-semibold': ['Nunito-SemiBold'],
+        'nunito-bold': ['Nunito-Bold'],
+
+        //Playwrite
+        'hand-thin': ['Playwrite-Thin'],
+        'hand-extralight': ['Playwrite-ExtraLight'],
+        'hand-light': ['Playwrite-Light'],
+        'hand': ['Playwrite-Regular'],
+      }
+    },
   },
   future: {
     hoverOnlyWhenSupported: true,
