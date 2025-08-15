1c4f45ddbfdede254e7e1f4db2454c2d4c1439e8
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-15 00:20:21 -0300

    refactor: reestruturação de diretórios

---
+++ banco-de-dados/modelo-fisico/schemas/4-storage/enums.sql
@@ -0,0 +1,29 @@
+CREATE TYPE
+app_utils.enum_s_storage_t_tb_bucket_c_nome
+AS ENUM (
+  'foto-perfil-usuario',
+  'foto-perfil-igreja',
+  'foto-perfil-ministerio',
+  'icone-instrumento',
+  'audio',
+  'pdf',
+  'sistema-icone-instrumento',
+  'sistema-imagem',
+  'sistema-audio'
+);
+
+CREATE TYPE
+app_utils.enum_s_storage_t_tb_arquivo_c_mime_type
+AS ENUM (
+  'image/png', 'image/jpeg', 'image/svg+xml',
+  'audio/mpeg', 'audio/wav', 'audio/ogg', 'audio/flac', 'audio/mp4', 'audio/x-alac',
+  'application/pdf'
+);
+
+CREATE TYPE
+app_utils.enum_s_storage_t_tb_arquivo_c_extensao
+AS ENUM (
+  'png', 'jpg', 'jpeg', 'svg',
+  'mp3', 'wav', 'ogg',  'flac', 'm4a', 'alac',
+  'pdf'
+);
--- banco-de-dados/modelo-fisico/schemas/5-church/enums.sql
+++ banco-de-dados/modelo-fisico/schemas/5-church/enums.sql
@@ -1,37 +1,3 @@
--- SCHEMA STORAGE
-CREATE TYPE
-app_utils.enum_s_storage_t_tb_bucket_c_nome
-AS ENUM (
-  'foto-perfil-usuario',
-  'foto-perfil-igreja',
-  'foto-perfil-ministerio',
-  'icone-instrumento',
-  'audio',
-  'pdf',
-  'sistema-icone-instrumento',
-  'sistema-imagem',
-  'sistema-audio'
-);
-
-CREATE TYPE
-app_utils.enum_s_storage_t_tb_arquivo_c_mime_type
-AS ENUM (
-  'image/png', 'image/jpeg', 'image/svg+xml',
-  'audio/mpeg', 'audio/wav', 'audio/ogg', 'audio/flac', 'audio/mp4', 'audio/x-alac',
-  'application/pdf'
-);
-
-CREATE TYPE
-app_utils.enum_s_storage_t_tb_arquivo_c_extensao
-AS ENUM (
-  'png', 'jpg', 'jpeg', 'svg',
-  'mp3', 'wav', 'ogg',  'flac', 'm4a', 'alac',
-  'pdf'
-);
-
-
-
--- SCHEMA CHURCH
 CREATE TYPE
 app_utils.enum_s_church_t_tb_igreja_c_denominacao
 AS ENUM (
