55e21946b6a25d5354659d70110ff3b3e8a10308
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-20 01:57:49 -0300

    refactor: alteração de nome de CK

--- banco-de-dados/modelo-fisico/schemas/1-auth/checks.sql
+++ banco-de-dados/modelo-fisico/schemas/1-auth/checks.sql
@@ -1,5 +1,13 @@
 ALTER TABLE auth.tb_usuario
-ADD CONSTRAINT ck_s_auth_t_tb_endereco_c_sexo
-CHECK (sexo IN (
-  'M', 'F'
-));
+ADD CONSTRAINT ck_s_auth_t_tb_usuario_c_cpf
+CHECK (
+  
+);
+
+ALTER TABLE auth.tb_usuario
+ADD CONSTRAINT ck_s_auth_t_tb_usuario_c_sexo
+CHECK (
+  sexo IN (
+    'M', 'F'
+  )
+);
