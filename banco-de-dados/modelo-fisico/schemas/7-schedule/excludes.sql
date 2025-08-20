7ac6977c03d3b28a5e53cd487c9839ac5a4b998d
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-20 01:36:55 -0300

    fix: correção de nome de campo desatualizado

--- banco-de-dados/modelo-fisico/schemas/7-schedule/excludes.sql
+++ banco-de-dados/modelo-fisico/schemas/7-schedule/excludes.sql
@@ -1,6 +1,6 @@
 ALTER TABLE schedule.tb_registro_ausencia
 ADD CONSTRAINT ex_s_schedule_t_tb_registro_ausencia_c_periodo
 EXCLUDE USING GIST (
-  usu_uuid WITH =,
+  s_auth_t_tb_usuario_c_lev WITH =,
   periodo WITH &&
 );
