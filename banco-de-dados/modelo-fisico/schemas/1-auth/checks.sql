a4d2b0ab62363ae82e4b6a3599fae2224fbbb30a
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-20 01:09:29 -0300

    refactor: troca de nomenclatura de "check" para "ck" em CHECK CONSTRAINTs

--- banco-de-dados/modelo-fisico/schemas/1-auth/checks.sql
+++ banco-de-dados/modelo-fisico/schemas/1-auth/checks.sql
@@ -1,6 +1,5 @@
 ALTER TABLE auth.tb_usuario
-ADD CONSTRAINT
-check_s_auth_t_tb_endereco_c_sexo
+ADD CONSTRAINT ck_s_auth_t_tb_endereco_c_sexo
 CHECK (sexo IN (
   'M', 'F'
 ));
--- banco-de-dados/modelo-fisico/schemas/3-storage/checks.sql
+++ banco-de-dados/modelo-fisico/schemas/3-storage/checks.sql
@@ -2,7 +2,7 @@
 -- dure de mais que zero a no máximo 5 minutos
 ALTER TABLE storage.tb_bucket
 ADD CONSTRAINT
-check_s_storage_t_tb_bucket_c_tempo_expiracao
+  ck_s_storage_t_tb_bucket_c_tempo_expiracao
 CHECK (
   tempo_expiracao_upload_em_segundos > 0 AND
   tempo_expiracao_upload_em_segundos <= 300
@@ -13,7 +13,7 @@ CHECK (
 -- 1 byte a 1 gigabyte
 ALTER TABLE storage.tb_bucket
 ADD CONSTRAINT
-check_s_storage_t_tb_bucket_c_tamanho_minimo_e_maximo
+  ck_s_storage_t_tb_bucket_c_tamanho_minimo_e_maximo
 CHECK (
   tamanho_minimo > 0 AND
   tamanho_minimo <= utils.conversor_mb_para_byte(1000) AND
