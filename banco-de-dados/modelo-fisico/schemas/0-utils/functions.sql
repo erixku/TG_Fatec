c8a66ec91de09f581bd6088589e853b15df26897
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-21 21:12:47 -0300

    feat: validador de CPF e inserção de exceções em outras funções

--- banco-de-dados/modelo-fisico/schemas/0-utils/functions.sql
+++ banco-de-dados/modelo-fisico/schemas/0-utils/functions.sql
@@ -1,12 +1,22 @@
 CREATE OR REPLACE FUNCTION
   utils.conversor_mb_para_byte(tamanho INTEGER)
 RETURNS INTEGER
-LANGUAGE sql
+LANGUAGE plpgsql
 IMMUTABLE
-SECURITY DEFINER
-SET search_path = storage, pg_temp
+SECURITY INVOKER
+SET search_path = pg_catalog
 AS $$
-  SELECT tamanho * 1024 * 1024
+BEGIN
+  IF tamanho IS NULL THEN
+    RAISE EXCEPTION 'O tamanho em megabytes não pode ser nulo';
+  END IF;
+
+  IF tamanho < 0 THEN
+    RAISE EXCEPTION 'O tamanho em megabytes não pode ser negativo';
+  END IF;
+
+  RETURN tamanho * 1024 * 1024;
+END;
 $$;
 
 
@@ -17,7 +27,7 @@ RETURNS VARCHAR(6)
 LANGUAGE plpgsql
 VOLATILE
 SECURITY DEFINER
-SET search_path = church, pg_temp
+SET search_path = church, pg_catalog
 AS $$
 DECLARE
   caracteres CONSTANT VARCHAR(62) := 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -52,3 +62,84 @@ BEGIN
   END LOOP;
 END;
 $$;
+
+
+
+CREATE OR REPLACE FUNCTION
+  utils.validador_cpf(cpf VARCHAR(11))
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+IMMUTABLE
+SECURITY INVOKER
+SET search_path = pg_catalog
+AS $$
+DECLARE
+  cpf_vetor TEXT[];
+  soma INT;
+  resto INT;
+  i INT;
+BEGIN
+  IF cpf IS NULL THEN
+    RAISE EXCEPTION 'CPF inválido: não pode ser nulo';
+  END IF;
+
+  cpf := trim(cpf);
+
+  IF cpf = '' THEN
+    RAISE EXCEPTION 'CPF inválido: não pode ser vazio';
+  END IF;
+
+  IF length(cpf) != 11 THEN
+    RAISE EXCEPTION 'CPF inválido: deve conter exatamente 11 dígitos';
+  END IF;
+
+  IF cpf ~ '[^0-9]' THEN
+    RAISE EXCEPTION 'CPF inválido: deve conter apenas números (sem pontos ou traços)';
+  END IF;
+
+  IF cpf ~ '^(\d)\1{10}$' THEN
+    RAISE EXCEPTION 'CPF inválido: não pode conter todos os dígitos iguais';
+  END IF;
+
+  -- converte o CPF em um vetor de caracteres
+  cpf_vetor := regexp_split_to_array(cpf, '');
+
+  -- Valida primeiro dígito verificador
+  soma := 0;
+  FOR i IN 1..9 LOOP
+    soma := soma + (cast(cpf_vetor[i] AS INTEGER) * (11 - i));
+  END LOOP;
+  resto := (soma * 10) % 11;
+  IF resto = 10 THEN
+    resto := 0;
+  END IF;
+  IF resto != cast(cpf_vetor[10] AS INTEGER) THEN
+    RAISE EXCEPTION 'CPF inválido: primeiro dígito verificador incorreto';
+  END IF;
+
+  -- Valida segundo dígito verificador
+  soma := 0;
+  FOR i IN 1..10 LOOP
+    soma := soma + (cast(cpf_vetor[i] AS INTEGER) * (12 - i));
+  END LOOP;
+  resto := (soma * 10) % 11;
+  IF resto = 10 THEN
+    resto := 0;
+  END IF;
+  IF resto != cast(cpf_vetor[11] AS INTEGER) THEN
+    RAISE EXCEPTION 'CPF inválido: segundo dígito verificador incorreto';
+  END IF;
+
+  RETURN TRUE;
+END;
+$$;
+
+
+
+
+
+-- CREATE OR REPLACE VIEW utils.view_teste AS
+--   SELECT id, cep, uf, cidade, bairro, rua, numero, complemento
+--   FROM auth.tb_endereco;
+
+-- SELECT * FROM utils.view_teste;
