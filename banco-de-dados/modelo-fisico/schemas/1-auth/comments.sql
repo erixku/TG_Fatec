46f024bf19cb4b544fcd6189cf4fc37db6c3f594
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-23 18:41:48 -0300

    docs: documentação das tabelas do Schema Auth com COMMENT ON

---
+++ banco-de-dados/modelo-fisico/schemas/1-auth/comments.sql
@@ -0,0 +1,12 @@
+COMMENT ON SCHEMA auth IS '
+  Schema destinado a armazenar dados pessoais dos usuários da aplicação,
+  dados de acesso e dados de autenticação
+';
+
+COMMENT ON TABLE auth.tb_endereco IS '
+  Tabela que armazena os dados do endereço de um usuário.
+';
+
+COMMENT ON TABLE auth.tb_usuario IS '
+  Tabela que armazena os dados pessoais e de acesso do usuário
+';
