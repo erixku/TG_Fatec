0b0dfc43766d0bb4dd0de9901b3c72f4d26170ac
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-23 12:34:00 -0300

    feat: validação de sexo (auth.tb_usuario)

---
+++ banco-de-dados/modelo-fisico/schemas/1-auth/validacoes/validador-sexo.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION
+  utils.s_auth_f_validador_sexo(sexo CHAR(1))
+RETURNS BOOLEAN
+LANGUAGE plpgsql
+IMMUTABLE
+SECURITY INVOKER
+SET search_path = pg_catalog
+AS $$
+BEGIN
+  IF sexo IS NULL THEN
+    RAISE EXCEPTION 'Sexo inválido: não pode ser nulo';
+  END IF;
+
+  IF sexo NOT IN ('M', 'F') THEN
+    RAISE EXCEPTION 'Sexo inválido: deve ser "M" (masculino) ou "F" (feminino)';
+  END IF;
+
+  RETURN TRUE;
+END;
+$$;
