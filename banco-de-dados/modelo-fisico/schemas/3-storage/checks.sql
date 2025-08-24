fdc71a719492f7f9fcad6ec63c7af165c5bd859e
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-24 17:03:57 -0300

    feat: adição de CK de nome de arquivo (storage)

--- banco-de-dados/modelo-fisico/schemas/3-storage/checks.sql
+++ banco-de-dados/modelo-fisico/schemas/3-storage/checks.sql
@@ -1,23 +1,23 @@
--- garante que o upload de arquivos
--- dure de mais que zero a no máximo 5 minutos
 ALTER TABLE storage.tb_bucket
-ADD CONSTRAINT
-  ck_s_storage_t_tb_bucket_c_tempo_expiracao
+ADD CONSTRAINT ck_s_storage_t_tb_bucket_c_tempo_expiracao
 CHECK (
   tempo_expiracao_upload_em_segundos > 0 AND
   tempo_expiracao_upload_em_segundos <= 300
 );
 
--- garante que o tamanho mínimo e máximo
--- dos arquivos esteja no intervalo de
--- 1 byte a 1 gigabyte
+-- garante que arquivos de buckets tenham de 1 byte a 1 gigabyte de tamanho
 ALTER TABLE storage.tb_bucket
-ADD CONSTRAINT
-  ck_s_storage_t_tb_bucket_c_tamanho_minimo_e_maximo
+ADD CONSTRAINT ck_s_storage_t_tb_bucket_c_tamanho_minimo_e_maximo
 CHECK (
   tamanho_minimo > 0 AND
   tamanho_minimo <= utils.conversor_mb_para_byte(1000) AND
   tamanho_maximo > 0 AND
   tamanho_maximo <= utils.conversor_mb_para_byte(1000) AND
   tamanho_maximo >= tamanho_minimo
+);
+
+ALTER TABLE storage.tb_arquivo
+ADD CONSTRAINT ck_s_storage_t_tb_arquivo_c_nome
+CHECK (
+  nome ~ '^[a-z0-9-]'
 );
