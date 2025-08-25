f7a5dfe7919780138f704efb6c6399570821ab16
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-24 23:30:21 -0300

    refactor: exclusão de comentário desnecessário

--- banco-de-dados/modelo-fisico/schemas/3-storage/indexes.sql
+++ banco-de-dados/modelo-fisico/schemas/3-storage/indexes.sql
@@ -1,6 +1,3 @@
--- garante unicidade entre ID de bucket, nome de arquivo e extensão de arquivo
--- para arquivos que não foram excluídos ainda, enquanto permite que arquivos
--- já excluídos possam ter ID de bucket, nome e extensão igual
 CREATE UNIQUE INDEX uq_s_storage_t_tb_arquivo_c_buc_id_c_nome_c_extensao
 ON storage.tb_arquivo (buc_id, nome, extensao)
 WHERE is_deletado = FALSE;
