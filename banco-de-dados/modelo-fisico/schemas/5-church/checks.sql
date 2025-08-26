0e1f5348fa7b903d894231d58f32d3a681ea1399
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-08-26 01:50:29 -0300

    feat: CKs de validações de descrições (church)

--- banco-de-dados/modelo-fisico/schemas/5-church/checks.sql
+++ banco-de-dados/modelo-fisico/schemas/5-church/checks.sql
@@ -3,3 +3,15 @@ ADD CONSTRAINT ck_s_church_t_tb_igreja_c_cnpj
 CHECK (
   utils.s_church_f_validador_cnpj(cnpj)
 );
+
+ALTER TABLE church.tb_atividade
+ADD CONSTRAINT ck_s_church_t_tb_atividade_c_descricao
+CHECK (
+  utils.s_church_f_validador_descricao(descricao, 50)
+);
+
+ALTER TABLE church.tb_igreja
+ADD CONSTRAINT ck_s_church_t_tb_ministerio_louvor_c_descricao
+CHECK (
+  utils.s_church_f_validador_descricao(descricao, 50)
+);
