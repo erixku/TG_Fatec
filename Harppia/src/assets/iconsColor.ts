5c618119f5dca1a8c586e353424a2c89626db56d
erixku <erickgomes1010@outlook.com>
2025-09-10 16:30:44 -0300

    Trocando o nome da pasta da aplicação para um nome generalista

--- Harppia/src/assets/iconsColor.ts
+++
@@ -1,6 +0,0 @@
-import { useColorScheme } from "react-native";
-
-const colorScheme = useColorScheme();
-    
-export const baseColor:string = colorScheme === 'dark' ? '#dbeafe' : '#0f172a';
-export const contrastColor:string = colorScheme === 'dark' ? '#93c5fd' : '#1d4ed8';
