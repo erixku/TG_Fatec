2408de78a3cfcb10a5697402d5f4cbb599118294
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-09-20 01:54:09 -0300

    refactor: deixando textos de enums em Lowercase

--- banco-de-dados/modelo-fisico/schemas/7-schedule/enums.sql
+++ banco-de-dados/modelo-fisico/schemas/7-schedule/enums.sql
@@ -1,8 +1,8 @@
 CREATE TYPE
   utils.enum_s_schedule_t_tb_registro_ausencia_c_motivo
 AS ENUM (
-  'Trabalho',
-  'Licença médica',
-  'Exame laboratorial',
-  'Outros'
+  'trabalho',
+  'licença médica',
+  'exame laboratorial',
+  'outros'
 );
