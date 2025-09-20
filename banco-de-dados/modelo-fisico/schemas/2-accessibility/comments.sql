908af10c35328fc3ab0e8e4a5a0a9d6520d117e4
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-09-20 14:20:50 -0300

    refactor: renomeando schema
    De "acessibility" para "accessibility"

---
+++ banco-de-dados/modelo-fisico/schemas/2-accessibility/comments.sql
@@ -0,0 +1,65 @@
+COMMENT ON SCHEMA accessibility IS '
+  Schema que armazena as configurações de acessibilidade do usuário.
+  Todas as tabelas recebem valores padrão no momento do cadastro, que podem ser,
+  posteriormente, personalizados pelo próprio usuário.
+';
+
+
+
+COMMENT ON TABLE accessibility.tb_intelectual IS '
+  Armazena as configurações de acessibilidade intelectual do usuário
+';
+
+COMMENT ON COLUMN accessibility.tb_intelectual.modo_foco IS '
+  Quando ativada, remove parte da estilização de componentes visuais
+  secundários na tela para destacar os componentes visuais principais
+';
+
+COMMENT ON COLUMN accessibility.tb_intelectual.feedback_imediato IS '
+  Quando ativada, permite que o sistema exiba mensagens temporárias
+  na parte inferior da tela que deem feedback imediato ao usuário sobre as
+  ações que ele realizou
+';
+
+
+
+COMMENT ON TABLE accessibility.tb_auditiva IS '
+  Armazena as configurações de acessibilidade auditiva do usuário
+';
+
+COMMENT ON COLUMN accessibility.tb_auditiva.flash IS '
+  Quando ativada, permite que o sistema acione flashes da cor tema da
+  aplicação (azul) na tela, em sincronia com as batidas do metrônomo,
+  na ferramenta de metrônomo
+';
+
+COMMENT ON COLUMN accessibility.tb_auditiva.intensidade_flash IS '
+  Define a intensidade visual dos flashes emitidos pelo modo
+  de flashes na tela
+';
+
+COMMENT ON COLUMN accessibility.tb_auditiva.vibracao_aprimorada IS '
+  Quando ativada, permite que o sistema acione vibrações
+  em sincronia com as batidas do metrônomo na ferramenta de metrônomo
+';
+
+COMMENT ON COLUMN accessibility.tb_auditiva.alertas_visuais IS '
+  Quando ativada, permite que o sistema substitua sons de
+  notificação por flashes de cor branca na tela
+';
+
+
+
+COMMENT ON TABLE accessibility.tb_visual IS '
+  Armazena as configurações de acessibilidade visual do usuário
+';
+
+COMMENT ON COLUMN accessibility.tb_visual.remover_animacoes IS '
+  Quando ativada, permite que o sistema desative todas as
+  animações da aplicação
+';
+
+COMMENT ON COLUMN accessibility.tb_visual.vibrar_ao_tocar IS '
+  Quando ativada, permite que o sistema acione vibração no dispositivo
+  móvel do usuário sempre que ele pressionar um item interativo em sua tela
+';
---
+++ banco-de-dados/modelo-fisico/schemas/2-accessibility/enums.sql
@@ -0,0 +1,19 @@
+CREATE TYPE
+  utils.enum_s_accessibility_t_tb_visual_c_cor_tema
+AS ENUM (
+  'claro',
+  'escuro'
+);
+
+CREATE TYPE
+  utils.enum_s_accessibility_t_tb_visual_c_daltonismo
+AS ENUM (
+  'tricromata',
+  'protanopia',
+  'protanomalia',
+  'deuteranopia',
+  'deuteranomalia',
+  'tritanopia',
+  'tritanomalia',
+  'acromatopsia'
+);
