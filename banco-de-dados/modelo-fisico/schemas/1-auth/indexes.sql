6c69e6e936d958793f3503dfcf4785aa670189b2
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-09-20 01:53:46 -0300

    refactor: atualizando nomes conforme nova modelagem

--- banco-de-dados/modelo-fisico/schemas/1-auth/indexes.sql
+++ banco-de-dados/modelo-fisico/schemas/1-auth/indexes.sql
@@ -1,12 +1,12 @@
 -- tb_usuario
 CREATE UNIQUE INDEX uq_s_auth_t_tb_usuario_c_cpf
 ON auth.tb_usuario (cpf)
-WHERE is_deletado = FALSE;
+WHERE is_deleted = FALSE;
 
 CREATE UNIQUE INDEX uq_s_auth_t_tb_usuario_c_email
 ON auth.tb_usuario (email)
-WHERE is_deletado = FALSE;
+WHERE is_deleted = FALSE;
 
 CREATE UNIQUE INDEX uq_s_auth_t_tb_usuario_c_telefone
 ON auth.tb_usuario (telefone)
-WHERE is_deletado = FALSE;
+WHERE is_deleted = FALSE;
--- banco-de-dados/modelo-fisico/schemas/3-storage/indexes.sql
+++ banco-de-dados/modelo-fisico/schemas/3-storage/indexes.sql
@@ -1,10 +1,10 @@
 -- tb_bucket
 CREATE UNIQUE INDEX uq_s_storage_t_tb_bucket_c_nome
 ON storage.tb_bucket (nome)
-WHERE is_deletado = FALSE;
+WHERE is_deleted = FALSE;
 
 
 -- tb_arquivo
 CREATE UNIQUE INDEX uq_s_storage_t_tb_arquivo_c_buc_id_c_nome_c_extensao
 ON storage.tb_arquivo (buc_id, nome, extensao)
-WHERE is_deletado = FALSE;
+WHERE is_deleted = FALSE;
