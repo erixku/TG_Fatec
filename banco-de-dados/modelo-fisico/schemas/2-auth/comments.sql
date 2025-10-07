aea41498cdd1f2bd2c19a88c0addec352eb29f47
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-10-07 00:22:04 -0300

    refactor: tornando strings em lowercase e ajustando espaçamentos

--- banco-de-dados/modelo-fisico/schemas/2-auth/comments.sql
+++ banco-de-dados/modelo-fisico/schemas/2-auth/comments.sql
@@ -2,10 +2,10 @@ COMMENT ON SCHEMA auth IS '
   Schema que armazena dados pessoais, de acesso e de autenticação dos usuários
 ';
 
-COMMENT ON TABLE auth.tb_endereco IS '
-  Tabela que armazena os dados do endereço dos usuários
-';
-
 COMMENT ON TABLE auth.tb_usuario IS '
   Tabela que armazena os dados pessoais e de acesso dos usuários
+';
+
+COMMENT ON TABLE auth.tb_endereco IS '
+  Tabela que armazena os dados do endereço dos usuários
 ';
--- banco-de-dados/modelo-fisico/schemas/5-song/enums.sql
+++ banco-de-dados/modelo-fisico/schemas/5-song/enums.sql
@@ -2,20 +2,20 @@ CREATE TYPE
   utils.enum_s_song_c_tonalidade
 AS ENUM (
   -- tonalidades maiores
-  'C', 'C#',
-  'D', 'D#',
-  'E',
-  'F', 'F#',
-  'G', 'G#',
-  'A', 'A#',
-  'B',
+  'c', 'c#',
+  'd', 'd#',
+  'e',
+  'f', 'f#',
+  'g', 'g#',
+  'a', 'a#',
+  'b',
 
   -- tonalidades menores
-  'Am', 'A#m',
-  'Bm',
-  'Cm', 'C#m',
-  'Dm', 'D#m',
-  'Em',
-  'Fm', 'F#m',
-  'Gm', 'G#m'
+  'am', 'a#m',
+  'bm',
+  'cm', 'c#m',
+  'dm', 'd#m',
+  'em',
+  'fm', 'f#m',
+  'gm', 'g#m'
 );
