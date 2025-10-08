d79d22d99a9910f66ca942b04295923d68d7fd93
Erick Gomes Barbosa <erickgomes1010@outlook.com>
2025-10-08 03:28:05 -0300

    Remoção da biblioteca de elementos compartilhados + Alteração da importação do construtor de SVG da logomarca

--- front-end/src/app/user/index.tsx
+++ front-end/src/app/user/index.tsx
@@ -3,7 +3,6 @@ import { View, useColorScheme, Pressable } from "react-native";
 import ThemedHarppiaLogo from "@/components/ThemedHarppiaLogo";
 import { CustomButton } from "@/components/CustomButtom";
 import { ArrowLeftIcon } from "react-native-heroicons/solid";
-import { SharedElement } from "react-navigation-shared-element";
 import { useRouter, useLocalSearchParams } from "expo-router";
 import Auth from "@/components/login&register/Auth";
 
@@ -19,33 +18,31 @@ export default function AuthScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-100 dark:bg-slate-800">
       <View className="flex p-10 h-[45rem] w-full max-w-[80%] justify-between items-center gap-y-4 rounded-xl bg-slate-50 shadow-md dark:bg-slate-700 dark:shadow-slate-400">
-        <SharedElement id="register-container">
-          <View className="flex-1 w-full justify-between items-strech gap-y-4">
-            {/* Cabeçalho */}
-            <View className="flex flex-row items-center justify-center gap-y-2">
-              <Pressable onPress={() => router.back()}>
-                <ArrowLeftIcon color={baseColor} />
-              </Pressable>
-              <View className="scale-75">
-                <ThemedHarppiaLogo
-                  baseColor={baseColor}
-                  contrastColor={contrastColor}
-                />
-              </View>
-            </View>
-
-            {/* Formulário */}
-            <Auth email={email} telefone={telefone} />
-
-            {/* Rodapé */}
-            <View className="flex justify-center flex-row gap-x-4 mt-5">
-              <CustomButton
-                label="Próximo"
-                onPress={() => {rota === "register"? router.push("/church") : router.push("/user/login/ResetPassword")}}
+        <View className="flex-1 w-full justify-between items-strech gap-y-4">
+          {/* Cabeçalho */}
+          <View className="flex flex-row items-center justify-center gap-y-2">
+            <Pressable onPress={() => router.back()}>
+              <ArrowLeftIcon color={baseColor} />
+            </Pressable>
+            <View className="scale-75">
+              <ThemedHarppiaLogo
+                baseColor={baseColor}
+                contrastColor={contrastColor}
               />
             </View>
           </View>
-        </SharedElement>
+
+          {/* Formulário */}
+          <Auth email={email} telefone={telefone} />
+
+          {/* Rodapé */}
+          <View className="flex justify-center flex-row gap-x-4 mt-5">
+            <CustomButton
+              label="Próximo"
+              onPress={() => {rota === "register"? router.push("/church") : router.push("/user/login/ResetPassword")}}
+            />
+          </View>
+        </View>
       </View>
     </View>
   );
