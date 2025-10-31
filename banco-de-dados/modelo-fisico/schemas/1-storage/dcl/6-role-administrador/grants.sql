c71968cfde28bb99e22f0fd12c043d03f75edaab
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-10-31 00:54:42 -0300

    feat: implementação de permissionamento (storage)

---
+++ banco-de-dados/modelo-fisico/schemas/1-storage/dcl/6-role-administrador/grants.sql
@@ -0,0 +1,55 @@
+GRANT USAGE ON SCHEMA storage TO role_administrador;
+
+
+-- tb_bucket
+GRANT
+  SELECT (
+    id,
+    is_deleted,
+    nome,
+    tempo_expiracao_upload_em_segundos,
+    tamanho_minimo,
+    tamanho_maximo
+  )
+  ON TABLE storage.tb_bucket
+  TO role_administrador;
+
+
+
+-- tb_arquivo
+GRANT
+  INSERT (
+    created_by,
+    nome,
+    mime_type,
+    extensao,
+    tamanho_em_bytes,
+    buc_id
+  )
+  ON TABLE storage.tb_arquivo
+  TO role_administrador;
+
+GRANT
+  SELECT (
+    id,
+    created_at,
+    deleted_at,
+    created_by,
+    deleted_by,
+    is_deleted,
+    nome,
+    extensao,
+    tamanho_em_bytes,
+    buc_id
+  )
+  ON TABLE storage.tb_arquivo
+  TO role_administrador;
+
+GRANT
+  UPDATE (
+    deleted_at,
+    deleted_by,
+    is_deleted
+  )
+  ON TABLE storage.tb_arquivo
+  TO role_administrador;
