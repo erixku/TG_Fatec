fea2262f759a7b7c8326bfd212f2b1d9a0248639
GustavoSouza417 <126614450+GustavoSouza417@users.noreply.github.com>
2025-10-31 00:55:08 -0300

    feat: implementação de permissionamento (auth)

---
+++ banco-de-dados/modelo-fisico/schemas/2-auth/dcl/1-role-anonimo/grants.sql
@@ -0,0 +1,55 @@
+GRANT USAGE ON SCHEMA auth TO role_anonimo;
+
+
+
+-- tb_usuario
+GRANT
+  INSERT (
+    cpf,
+    nome,
+    sobrenome,
+    nome_social,
+    sobrenome_social,
+    sexo,
+    data_nascimento,
+    email,
+    telefone,
+    senha,
+    s_storage_t_tb_arquivo_c_foto
+  )
+  ON TABLE auth.tb_usuario
+  TO role_anonimo;
+
+GRANT
+  SELECT (
+    id,
+    cpf,
+    email,
+    telefone,
+    senha
+  )
+  ON TABLE auth.tb_usuario
+  TO role_anonimo;
+
+GRANT
+  UPDATE (
+    senha
+  )
+  ON TABLE auth.tb_usuario
+  TO role_anonimo;
+
+
+
+-- tb_endereco
+GRANT
+  INSERT (
+    cep,
+    uf,
+    cidade,
+    bairro,
+    logradouro,
+    numero,
+    complemento
+  )
+  ON TABLE auth.tb_endereco
+  TO role_anonimo;
---
+++ banco-de-dados/modelo-fisico/schemas/2-auth/dcl/2-role-usuario/grants.sql
@@ -0,0 +1,72 @@
+GRANT USAGE ON SCHEMA auth TO role_usuario;
+
+
+
+-- tb_usuario
+GRANT
+  SELECT (
+    id,
+    is_deleted,
+    cpf,
+    nome,
+    sobrenome,
+    nome_social,
+    sobrenome_social,
+    sexo,
+    data_nascimento,
+    email,
+    telefone,
+    senha,
+    s_storage_t_tb_arquivo_c_foto
+  )
+  ON TABLE auth.tb_usuario
+  TO role_usuario;
+
+GRANT
+  UPDATE (
+    updated_at,
+    deleted_at,
+    last_access,
+    is_deleted,
+    nome,
+    sobrenome,
+    nome_social,
+    sobrenome_social,
+    data_nascimento,
+    telefone,
+    senha,
+    s_storage_t_tb_arquivo_c_foto
+  )
+  ON TABLE auth.tb_usuario
+  TO role_usuario;
+
+
+
+-- tb_endereco
+GRANT
+  SELECT (
+    id,
+    cep,
+    uf,
+    cidade,
+    bairro,
+    logradouro,
+    numero,
+    complemento
+  )
+  ON TABLE auth.tb_endereco
+  TO role_usuario;
+
+GRANT
+  UPDATE (
+    updated_at,
+    cep,
+    uf,
+    cidade,
+    bairro,
+    logradouro,
+    numero,
+    complemento
+  )
+  ON TABLE auth.tb_endereco
+  TO role_usuario;
