2adfe06d2d06d709359a82759c50cfe6aecb0489
LucasDiabloChan <bomdiao53@gmail.com>
2025-11-12 17:25:34 -0300

    Ajustes no dockerfile para deploy

--- back-end/endpoints/src/main/java/br/app/harppia/modulo/notification/application/usecases/SalvarNotificacaoUseCase.java
+++ back-end/endpoints/src/main/java/br/app/harppia/modulo/notification/application/usecases/SalvarNotificacaoUseCase.java
@@ -3,6 +3,6 @@ package br.app.harppia.modulo.notification.application.usecases;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CriarNotificacaoUseCase {
+public class SalvarNotificacaoUseCase {
 
 }
--- back-end/endpoints/src/main/java/br/app/harppia/modulo/notification/infrastructure/repository/NotificacaoRepository.java
+++ back-end/endpoints/src/main/java/br/app/harppia/modulo/notification/infrastructure/repository/NotificacaoRepository.java
@@ -1,5 +1,8 @@
 package br.app.harppia.modulo.notification.infrastructure.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface NotificacaoRepository {
 
 }
